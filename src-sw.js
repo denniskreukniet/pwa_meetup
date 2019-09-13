f6c6b31045ee3de8191c7a59b163dfb476e99ec6
dennis kreukniet <>
2019-09-13 17:33:53 +0200

    Changed caching strategy and made api call generic.

--- src-sw.js
+++ src-sw.js
@@ -1,6 +1,17 @@
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 workbox.routing.registerRoute(
-    /https:\/\/pluralsight\-pwa\-scratch\.firebaseio\.com\/flights\.json/,
-    new workbox.strategies.NetworkFirst()
+    /https:\/\/pluralsight\-pwa\-scratch\.firebaseio\.com\/*\.json/,
+    new workbox.strategies.CacheFirst({
+        cacheName: 'api-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                // Only cache requests for a week
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+                // Only cache 50 requests.
+                maxEntries: 50,
+            }),
+        ]
+
+    })
 );
